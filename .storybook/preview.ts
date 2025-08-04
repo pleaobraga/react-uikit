dc9075a48968c38a22d12b57e6e79b2fd87433d6
pleaobraga <pleao.braga@gmail.com>
2025-08-04 09:15:21 -0300

    feat: add dark theme mode to storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,6 @@
-import type { Preview } from '@storybook/react-vite'
+import type { Preview, ReactRenderer } from '@storybook/react-vite'
+import { withThemeByClassName } from '@storybook/addon-themes'
+
 import '../src/index.css'
 
 const preview: Preview = {
@@ -17,6 +19,15 @@ const preview: Preview = {
       test: 'todo',
     },
   },
+  decorators: [
+    withThemeByClassName<ReactRenderer>({
+      themes: {
+        light: '',
+        dark: 'dark',
+      },
+      defaultTheme: 'dark',
+    }),
+  ],
 }
 
 export default preview
