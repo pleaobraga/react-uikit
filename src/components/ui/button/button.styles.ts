e852fab19d915fd065104d25ddbbe9e64991ad7b
pleaobraga <pleao.braga@gmail.com>
2025-09-21 17:33:40 -0300

    feat: update button sizes

--- src/components/ui/button/button.styles.ts
+++ src/components/ui/button/button.styles.ts
@@ -3,8 +3,8 @@ import { cva } from 'class-variance-authority'
 
 export const buttonVariants = cva(
   cn(
-    'inline-flex items-center justify-center gap-2',
-    'whitespace-nowrap rounded-md text-sm font-medium',
+    'inline-flex items-center justify-center',
+    'whitespace-nowrap rounded-sm',
     'transition-all disabled:pointer-events-none disabled:opacity-50',
     "[&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0",
     'outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
@@ -27,15 +27,18 @@ export const buttonVariants = cva(
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default: 'h-9 px-4 py-2 has-[>svg]:px-3',
-        sm: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
-        lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
+        xs: 'text-xs/5 px-2',
+        sm: 'text-sm/6 px-2',
+        base: 'text-base/7 px-3',
+        lg: 'text-lg/8 px-3',
+        xl: 'text-xl/9 px-3',
         icon: 'size-9',
       },
+      compact: { true: 'px-2 py-4', false: '' },
     },
     defaultVariants: {
       variant: 'default',
-      size: 'default',
+      size: 'base',
     },
   }
 )
