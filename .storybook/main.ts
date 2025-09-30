02452d1ca659fdca4146615cf533f247b5b85158
pleaobraga <pleao.braga@gmail.com>
2025-09-30 10:27:01 -0300

    chore: set up publish on GH pages

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,11 +8,14 @@ const config: StorybookConfig = {
     '@storybook/addon-onboarding',
     '@storybook/addon-a11y',
     '@storybook/addon-vitest',
-    '@storybook/addon-themes',
+    '@storybook/addon-themes'
   ],
   framework: {
     name: '@storybook/react-vite',
-    options: {},
+    options: {}
   },
+  viteFinal: async (config) => {
+    return { ...config, base: '/react-uikit/' }
+  }
 }
 export default config
